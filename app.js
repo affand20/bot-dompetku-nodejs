eef6523556d39cbe024b418f662cff1eda64b9aa
Affandy Fahrizain <fahri.lafa@gmail.com>
2018-09-24 22:46:26 +0700

    bismillah terus

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const line = require('@line/bot-sdk');
 
-// const port = 3000;
+const port = 3001;
 
 const config = {
     channelAccessToken : 'lxDSFG57mPcz6g6uiyJJxWVMTMf2ud6xKhHDgSHf8Zj3af3OwD/2PvZewztMgP87rdgD697ZahJU/v2uLQcNNIx/GCW1ijK/S2sqQbfx55Y5lSQ1dt17CdnbCaN761Hil2L7Vhv8kekCLAg0wMiUGAdB04t89/1O/w1cDnyilFU=',
@@ -11,7 +11,7 @@ const config = {
 
 const app = express();
 app.get('/' , (req, res) => res.send("Hello World"));
-// app.listen(port, () => console.log(`Listening on port ${port}!`));
+app.listen(port || 5000, () => console.log(`Listening on port ${port}!`));
 app.post('/webhook', line.middleware(config), (req, res) => {
     Promise
     .all(req.body.events.map(handleEvent))
@@ -20,23 +20,20 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 const client = new line.Client(config);
 function handleEvent(event) {
-    if (event.type !== 'message' || event.message.text !== 'text') {
-        var text = event.message.text;
-        console.log(text);
-        // if (text == '/test') {
-        //     return client.replyMessage(event.replyToken, {
-        //         type: 'text',
-        //         text: text
-        //     });
-        // } else{
-        //     return Promise.resolve(null);
-        // }
+    if (event.type === 'message') {
+        const message = event.message;
+
+        if (message.type == 'text') {
+            console.log('masuk coy');
+            
+            return client.replyMessage(event.replyToken, {
+                "type":"text",
+                "text":"H3h3h3"
+            });
+        }
+    } else{
         return Promise.resolve(null);
     }
-    return client.replyMessage(event.replyToken, {
-        type: text,
-        text: "balasanku : "+event.message.text
-    });
 }
 
 
